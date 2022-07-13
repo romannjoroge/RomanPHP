e3400667bc038be28d7ad5408f243c54318409a6
Roman Njoroge <roman.njoroge@njuguna.com>
2022-07-13 19:52:54 +0300

    Sending data between PHP files with forms

---
+++ 5-forms_and_datapassing/form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>This is my Form</h1>
+        <form action="action.php" method="POST">
+            <input type="text" placeholder="Name..." id="name">
+            <label for="name">Name</label>
+            <button type="submit">Submit</button>
+        </form>
+    </body>
+</html>
