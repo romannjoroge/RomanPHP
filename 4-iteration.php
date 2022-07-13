835cb298d24df10b132bf0a72d925261e49ce632
Roman Njoroge <roman.njoroge@njuguna.com>
2022-07-13 19:52:03 +0300

    For loop, while loop and foreach loop in PHP

---
+++ 4-iteration.php
@@ -0,0 +1,13 @@
+<?php
+    for ($i = 0; $i < 20; $i++){
+        echo "The value for i is ${i}\n";
+    }
+
+    echo "While loop\n";
+
+    $i = 0;
+    while($i < 20){
+        echo "The value of i is ${i}\n";
+        $i++;
+    }
+?>
