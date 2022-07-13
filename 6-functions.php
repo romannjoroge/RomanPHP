dd9c4889b409831db916521c4f880857d79cef6e
Roman Njoroge <roman.njoroge@njuguna.com>
2022-07-13 19:57:36 +0300

    Functions in PHP

---
+++ 6-functions.php
@@ -0,0 +1,12 @@
+<?php
+    /* 
+        Functions are defined as follows:
+        function <function-name>(<parameters>){
+            <function-body>
+        }
+    */
+    function greet($name){
+        echo "Hello ${name}\nYou're looking good today!\n";
+    }
+    greet("Bob");
+?>
