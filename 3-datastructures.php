6f567008defc786a8a5f45542b2666a57ce87b46
Roman Njoroge <roman.njoroge@njuguna.com>
2022-07-13 17:32:58 +0300

    Datastructures in PHP

---
+++ 3-datastructures.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+        $<list-name> = [<value1>, <value2>, ..... , <value-n>]
+        Lists in PHP can accept values of any data-type
+
+        Lists are indexed like this: $<list-name>[index]
+
+        $<dict-name> = ["key"=>value, "key2"=>value2, ... , "keyn"=>valuen ]
+        $<dict-name>["key"]
+    */
+?>
